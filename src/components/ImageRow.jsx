da417d9500f26c00821206a22526ba5eaf4c8e1b
Deeshma Deepak <deeshmadas@gmail.com>
2025-08-20 21:06:17 +0530

    Fix image import path for Vercel build

--- src/components/ImageRow.jsx
+++ src/components/ImageRow.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import food1 from "../assets/food1.jpg";
-import food2 from "../assets/food2.jpg";
-import food3 from "../assets/food3.jpg";
-import food4 from "../assets/food4.jpg";
+import food1 from "../assets/Food1.jpg";
+import food2 from "../assets/Food2.jpg";
+import food3 from "../assets/Food3.jpg";
+import food4 from "../assets/Food4.jpg";
 
 const ImageRow = () => {
   const [foodImages] = useState([food1, food2, food3, food4]);
